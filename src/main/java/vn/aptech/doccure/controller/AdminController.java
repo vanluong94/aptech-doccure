a1d86122ffe4efa19ca4c9f927c0ad21c24c85e2
Van Luong <van.luong.94114@gmail.com>
2022-04-07 18:22:52 +0700

    [fix] assets path on template and [update] some urls

--- src/main/java/vn/aptech/doccure/controller/AdminController.java
+++ src/main/java/vn/aptech/doccure/controller/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("admin")
 public class AdminController {
-    @GetMapping("/")
+    @GetMapping("")
     public String dashboard() {
         return "admin/pages/dashboard";
     }
