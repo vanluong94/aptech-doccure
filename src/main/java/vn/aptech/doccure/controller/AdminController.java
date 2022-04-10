ba252aab40fffe4ae9b40622ed037a99ef303e8f
Van Luong <van.luong.94114@gmail.com>
2022-04-10 10:05:57 +0700

    [update] dashboard for all roles

--- src/main/java/vn/aptech/doccure/controller/AdminController.java
+++ src/main/java/vn/aptech/doccure/controller/AdminController.java
@@ -1,14 +1,14 @@
 package vn.aptech.doccure.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("admin")
+@RequestMapping("dashboard")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
-    @GetMapping("")
-    public String dashboard() {
-        return "admin/pages/dashboard";
-    }
+
 }
--- src/main/java/vn/aptech/doccure/controller/HomeController.java
+++ src/main/java/vn/aptech/doccure/controller/HomeController.java
@@ -1,7 +1,12 @@
 package vn.aptech.doccure.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.server.ResponseStatusException;
+import vn.aptech.doccure.common.Constants;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
@@ -10,4 +15,18 @@ public class HomeController {
     public String home() {
         return "pages/home";
     }
+
+    @GetMapping("dashboard")
+    public String dashboard(HttpServletRequest request) {
+        if (request.isUserInRole(Constants.Roles.ROLE_DOCTOR)) {
+            return "pages/doctorDashboard";
+        } else if (request.isUserInRole(Constants.Roles.ROLE_PATIENT)) {
+            return "pages/patientDashboard";
+        } else if (request.isUserInRole(Constants.Roles.ROLE_ADMIN)) {
+            return "admin/pages/dashboard";
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
 }
