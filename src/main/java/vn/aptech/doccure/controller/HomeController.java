6dbee5b1a90c49edf3ec12ec9c1060ba3158ccc8
Van Luong <van.luong.94114@gmail.com>
2022-06-16 23:22:10 +0700

    [update] doctor item layout on homepage

--- src/main/java/vn/aptech/doccure/controller/HomeController.java
+++ src/main/java/vn/aptech/doccure/controller/HomeController.java
@@ -7,13 +7,14 @@ import org.springframework.web.servlet.ModelAndView;
 import vn.aptech.doccure.common.Constants;
 import vn.aptech.doccure.entities.Role;
 import vn.aptech.doccure.entities.Speciality;
-import vn.aptech.doccure.entities.User;
+import vn.aptech.doccure.model.DoctorDTO;
 import vn.aptech.doccure.service.RoleService;
 import vn.aptech.doccure.service.SpecialityService;
 import vn.aptech.doccure.service.UserService;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
@@ -36,7 +37,9 @@ public class HomeController {
          */
         Set<Role> roles = new HashSet<>();
         roles.add(roleService.findByName(Constants.Roles.ROLE_DOCTOR));
-        Iterable<User> doctors = userService.findTop10ByRolesInOrderByIdDesc(roles);
+        Iterable<DoctorDTO> doctors = userService.findTop10ByRolesInOrderByIdDesc(roles)
+                .stream().map(DoctorDTO::from)
+                .collect(Collectors.toList());
         Iterable<Speciality> specialities = specialityService.findAllByOrderByIdDesc();
         modelAndView.addObject("doctors", doctors);
         modelAndView.addObject("specialities", specialities);
